9c417a02a1f206e55f69ef5bbf3f56f7590667eb
James Trimarco <james.trimarco@gmail.com>
2019-09-16 14:29:04 -0400

    creates eda schema

--- sql/create_schemas.sql
+++ sql/create_schemas.sql
@@ -2,6 +2,7 @@ CREATE SCHEMA if not exists raw;
 -- CREATE SCHEMA if not exists gis;
 CREATE SCHEMA if not exists cleaned;
 CREATE SCHEMA if not exists semantic;
+CREATE SCHEMA if not exists eda;
 -- CREATE SCHEMA if not exists model;
 -- CREATE SCHEMA if not exists results;
 -- CREATE SCHEMA if not exists vis;
