7202ae4650572e0e1746094047c4d625771ce9b0
James Trimarco <james.trimarco@gmail.com>
2019-09-20 22:20:06 -0400

    moves json-to-csv out of utils

--- src/utils.py
+++ src/utils.py
@@ -90,66 +90,6 @@ def load_yaml(filename):
     return yaml_contents
 
 
-def json_directory_to_csv(temp_subdir, json_subdir, start_end_days):
-    """
-    Converts AIS JSON files from data folder to CSV files in the temporary folder.
-
-    Parameters:
-    temp_subdir: OS specific path
-        Path to the subdirectory to store the csvs in
-    json_subdir: OS specific pth
-        Path to a subdirectory with some subset of the data
-
-    Returns:
-    json_counter: int
-        Records count of processed json files
-
-    """
-
-    # Obtain all json files within subdirectory
-    json_files = json_subdir.glob('**/*.json')
-
-    json_counter = 0
-    for json_path in json_files:
-        #  Finds the two digits just before the underscore
-        #  These digits represent the day of the month in the filename
-        match: str = re.search('([0-9]){2}(?=_)', json_path.name)
-        day_of_month = int(match.group(0))
-        if day_of_month < start_end_days[0] or day_of_month > start_end_days[1]:
-            continue
-
-        print(f"Processing {json_path}")
-
-        json_counter += 1
-
-        with open(json_path) as infile:
-            # TODO: do it in spark instead
-            data = json.load(infile)
-
-        with open((temp_subdir / json_path.stem).with_suffix('.csv'), 'w', newline='') as csvfile:
-            csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC)
-            #  TODO: remove all '0x00' characters
-
-            for i, segment in enumerate(data):
-
-                # Skip Userinfo row
-                if i == 0:
-                    continue
-
-                else:
-                    for j, observation in enumerate(segment):
-
-                        # Write Header
-                        if j == 0:
-                            csvwriter.writerow(observation.keys())
-
-                        else:
-                            csvwriter.writerow(observation.values())
-
-    time.sleep(1)
-    return json_counter
-
-
 def execute_sql(string, engine, read_file, print_=False, return_df=False, chunksize=None, params=None):
     """
     Executes a SQL query from a file or a string using SQLAlchemy engine
