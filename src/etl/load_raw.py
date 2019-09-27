9b583658d04ad7f3eb2327e083b794f1b99f10ea
James Trimarco <james.trimarco@gmail.com>
2019-09-27 18:46:42 -0400

    puts iuu list into long format and uploads to postgres

--- src/etl/load_raw.py
+++ src/etl/load_raw.py
@@ -2,6 +2,7 @@ from utils import copy_csv_to_db
 import subprocess
 import os
 
+
 def load_csv(dir, engine, out_table):
     """
     Load csv files to database
@@ -54,3 +55,46 @@ def load_shp(DATA_DIR, dir_dict, credentials_dict):
         print("Uploading file {}".format(shapefile))
         subprocess.call(command, shell=True)
         print("Done")
+
+
+def load_iuu_list(filename, engine):
+    iuu_list_orig = Path.cwd().parent.parent.joinpath('aux_data').joinpath(filename)
+    pd_test = pd.read_csv(iuu_list_orig, delimiter="\t", encoding="ISO-8859-1")
+
+    pd_test.rename(columns={'IOTC': 'Date_IOTC',
+                            'ICCAT': 'Date_ICCAT',
+                            'IATTC': 'Date_IATTC',
+                            'WCPFC': 'Date_WCPFC',
+                            'CCAMLR': 'Date_CCAMLR',
+                            'SEAFO': 'Date_SEAFO',
+                            'INTERPOL': 'Date_INTERPOL',
+                            'NEAFC': 'Date_NEAFC',
+                            'NAFO': 'Date_NAFO',
+                            'SPRFMO': 'Date_SPRFMO',
+                            'CCSBT': 'Date_CCSBT',
+                            'GFCM': 'Date_GFCM',
+                            'NPFC': 'Date_NPFC',
+                            'SIOFA': 'Date_SIOFA',
+                            'Reason': 'Reason_IOTC',
+                            'Reason.1': 'Reason_ICCAT',
+                            'Reason.2': 'Reason_IATTC',
+                            'Reason.3': 'Reason_WCPFC',
+                            'Reason.4': 'Reason_CCAMLR',
+                            'Reason.5': 'Reason_SEAFO',
+                            'Reason.6': 'Reason_INTERPOL',
+                            'Reason.7': 'Reason_NEAFC',
+                            'Reason.8': 'Reason_NAFO',
+                            'Reason.9': 'Reason_SPRFMO',
+                            'Reason.10': 'Reason_CCSBT',
+                            'Reason.11': 'Reason_GFCM',
+                            'Reason.12': 'Reason_NPFC',
+                            'Reason.13': 'Reason_SIOFA',
+                            }, inplace=True)
+
+    pd_test = pd_test.reset_index()
+    long = pd.wide_to_long(pd_test, i='index', j='Violation', stubnames=['Reason', 'Date'], sep="_", suffix='\\w+')
+
+    long = long.dropna(how='all', subset=['Reason', 'Date']).reset_index()
+    long = long.drop('index', axis=1)
+
+    copy_csv_to_db(long, iuu_list, engine, header=True, sep=',')
