0e1e0953c477ba95641f8a6e2b976694d363da0d
James Trimarco <james@tpt.(none)>
2019-09-27 16:35:24 -0400

    sets up config

--- src/spark_dedupe.py
+++ src/spark_dedupe.py
@@ -13,46 +13,54 @@ import pyspark
 start = datetime.datetime.now()
 
 # Configure Spark
-conf = pyspark.SparkConf()
+conf = pyspark.SparkConf().setAll([('spark.local.dir', ['/Akamai/tmp_spark', '/Akamai_scratch/tmp_spark'])])
 conf.set('spark.local.dir', '/Akamai/tmp_spark/')
-conf.set('spark.executor.memory', '15g')
-conf.set('spark.driver.memory', '15g')
+conf.set('spark.executor.memory', '200g')
+conf.set('spark.driver.memory', '200g')
+conf.set('spark.worker.dir', '/Akamai')
+conf.set("spark.sql.shuffle.partitions", "2500")
 
 # Tell Spark to use all the local clusters
-sc = pyspark.SparkContext('local[*]', 'ais', conf)
+sc = pyspark.SparkContext('local[*]', 'airports', conf)
 
 # Tell spark to create a session
 from pyspark.sql import SparkSession
 
-# sess = SparkSession(sc).builder.config(sc.getConf).config("spark.local.dir", "/Akamai_scratch/").getOrCreate()
+sess = SparkSession.builder.config(conf=conf).getOrCreate()
 sess = SparkSession(sc)
 
+print(sc._conf.getAll())
 # Hold back on the error messages
 sc.setLogLevel("ERROR")
 
 all_csv_files = Path('/Akamai/ais_project_data/ais_csv_files/')
+sep_csv_files = Path('/Akamai/ais_project_data/ais_csv_files/2019Jun/*.csv')
 
 for monthly_dir in all_csv_files.glob('*/'):
-    csvs_with_dupes = monthly_dir.glob('*.csv')
+    csvs_with_dupes = monthly_dir.joinpath('*.csv')
 
-    raw_data = sc.textFile(csvs_with_dupes) \
-        .map(lambda line: line.split(',')) \
-        .map(lambda x: [z.strip('\"') for z in x])
-
+    raw_data = sc.textFile(str(sep_csv_files.resolve())) \
+        .map(lambda line: tuple(z.strip('\"') for z in line.split(','))) 
+    
+    
     # Define header as first row
     header = raw_data.first()
     # Remove header
     ais_with_dupes = raw_data.filter(lambda x: x != header)
-    print("Name of subdirectory: ", subpath.name)
+    raw_data.unpersist()
+
+    print(ais_with_dupes.take(5))
+    print("Name of subdirectory: ", monthly_dir.name)
     print("Rows in original data: ", ais_with_dupes.count())
 
     # Remove reviews that share mmsi and time
     # Note that the entire row is the value here
-    all_dupes = ais_with_dupes.map(lambda x: ((x[0], x[1]), x))
+    #all_dupes = ais_with_dupes.map(lambda x: ((x[0], x[1]), x))
     # Group by row values, dropping duplicates
-    ais_deduped = all_dupes.reduceByKey(lambda x, y: x) \
-        .map(lambda x: x[1])
-
+    #ais_deduped = all_dupes.reduceByKey(lambda x, y: x) \
+    #    .map(lambda x: x[1])
+    ais_deduped = ais_with_dupes.distinct()
+    ais_with_dupes.unpersist()
     print("Rows in deduped data: ", ais_deduped.count())
 
 
@@ -63,7 +71,7 @@ for monthly_dir in all_csv_files.glob('*/'):
     lines = ais_deduped.map(toCSVLine)
 
     deduped_path = Path('/Akamai/ais_project_data/ais_deduped')
-    save_path = deduped_path.joinpath(mothly_dir.name)
+    save_path = deduped_path.joinpath('2019Jun')
 
     if save_path.is_dir():
         remove_dir(save_path)
