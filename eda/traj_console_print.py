467132e65e6be79dd1c39f65f1825a1e1255fcfe
James Trimarco <james.trimarco@gmail.com>
2019-10-09 21:25:27 -0400

    pycharm PEP8 cleanup

--- eda/traj_console_print.py
+++ eda/traj_console_print.py
@@ -41,54 +41,53 @@ def get_color(r, g, b):
     ansi_string (str): String needed to print color in console
   
     """
-    return f"\x1b[48;5;{int(get_ansi_color_code(r,g,b))}m \x1b[0m"
+    return f"\x1b[48;5;{int(get_ansi_color_code(r, g, b))}m \x1b[0m"
 
 
-def show_image(flat_trajectory, resize = True):
-    """ 
+def show_image(flat_trajectory, resize=False):
+    """
     Takes a trajectory and prints into console.
-  
-    Parameters: 
+
+    Parameters:
     flat_trajectory (list): List containing trajectory of boat
     resize (bool): Resizes printing for readability
-  
+
     """
-    
+
     # Attempt converting from flat trajectory to image
     try:
         img = flat_trajectory
-        
+
         # Modular way to determine resizing for square image
-        reshape_dim = int(len(img)**(1./2))
-        
+        reshape_dim = int(len(img) ** (1. / 2))
+
         # Converts flat array to grayscale
-        grayscale_img = np.where(np.array(img)>0,255,0).reshape(-1, reshape_dim)
-        
+        grayscale_img = np.where(np.array(img) > 0, 255, 0).reshape(-1, reshape_dim)
+
         # Converts grayscale image to rgb
-        rgb_img = np.stack((grayscale_img,)*3, axis=-1)
-    
+        rgb_img = np.stack((grayscale_img,) * 3, axis=-1)
+
     # EXIT IF TRAJECTORY ERROR
     except:
         exit('Trajectory format INCORRECT')
-    
+
     # Halve the size of print output for readibility
     if resize:
-        h = int(reshape_dim*.5)
+        h = int(reshape_dim * .5)
         w = h
-        rgb_img.resize((h,w,3))
+        rgb_img.resize((h, w, 3))
 
     # Obtain final dimensions of image
-    h,w,c = rgb_img.shape
+    h, w, c = rgb_img.shape
 
     # For every height component
     for x in range(h):
-        
+
         # For every width component
         for y in range(w):
-            
             # Obtain pixel rgb values
             pix = rgb_img[x][y]
-            
+
             # Print in ansi color scheme for console
             print(get_color(pix[0], pix[1], pix[2]), sep='', end='')
             print(get_color(pix[0], pix[1], pix[2]), sep='', end='')
