c66f93189f0bd2abbd16fecdd72b61ae3c4d9b6d
Path-A <36173484+Path-A@users.noreply.github.com>
2019-11-27 14:07:29 -0500

    #60 Early Stopping + Results csv + Class index fix
    Added early stopping after 3 failed val_loss reductions. Now outputs a results csv. Fixed an issue where 0 represented fishing and 1 represented no_fishing.

--- src/modeling/train_cnn.py
+++ src/modeling/train_cnn.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from pathlib import Path
 import random
 from sklearn.metrics import roc_curve
@@ -9,6 +10,7 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping
 
 
 def run_cnn(split_directory, batchsize=256, epochs=50, color_mode='rgb'):
@@ -21,12 +23,6 @@ def run_cnn(split_directory, batchsize=256, epochs=50, color_mode='rgb'):
     # Image dimensions
     IMG_HEIGHT = 128
     IMG_WIDTH = 128
-    
-    # Check color mode for channels
-    if color_mode=='rgb':
-        IMG_DEPTH = 3
-    else:
-        IMG_DEPTH = 1
 
     # Model hyperparameters
     batch_size = batchsize
@@ -41,7 +37,7 @@ def run_cnn(split_directory, batchsize=256, epochs=50, color_mode='rgb'):
                                                                shuffle=True,
                                                                target_size=(IMG_HEIGHT, IMG_WIDTH),
                                                                class_mode='binary',
-                                                               color_mode=color_mode)
+                                                               color_mode=color_mode, classes={'no_fishing': 0, 'fishing': 1})
 
     # Test generator parameters
     test_image_generator = ImageDataGenerator(rescale=1./255)
@@ -51,11 +47,11 @@ def run_cnn(split_directory, batchsize=256, epochs=50, color_mode='rgb'):
                                                                   directory=test_dir,
                                                                   target_size=(IMG_HEIGHT, IMG_WIDTH),
                                                                   class_mode='binary',
-                                                                  color_mode=color_mode)
+                                                                  color_mode=color_mode, classes={'no_fishing': 0, 'fishing': 1}, shuffle=False)
 
     # Creating basic tf.keras sequential model
     model = Sequential([
-        Conv2D(8, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,IMG_DEPTH)),
+        Conv2D(8, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
         Conv2D(8, 3, padding='same', activation='relu'),
         MaxPooling2D(),
         Conv2D(16, 3, padding='same', activation='relu'),
@@ -70,6 +66,9 @@ def run_cnn(split_directory, batchsize=256, epochs=50, color_mode='rgb'):
         Dense(1, activation='sigmoid')
     ])
 
+    # Early stopping
+    earlystop = EarlyStopping(monitor='val_loss', patience = 3)
+
     # Compile the model
     model.compile(optimizer='adam',
                   loss='binary_crossentropy',
@@ -82,7 +81,7 @@ def run_cnn(split_directory, batchsize=256, epochs=50, color_mode='rgb'):
     history = model.fit_generator(
         train_data_gen,
         epochs = epochs,
-        validation_data = test_data_gen
+        validation_data = test_data_gen, callbacks=[earlystop] 
     )
 
     # Store fit history for plotting
@@ -90,7 +89,7 @@ def run_cnn(split_directory, batchsize=256, epochs=50, color_mode='rgb'):
     val_acc = history.history['val_accuracy']
     loss = history.history['loss']
     val_loss = history.history['val_loss']
-    epochs_range = range(epochs)
+    epochs_range = range(earlystop.stopped_epoch + 1)
 
     # Plot the training/test accuracy and loss
     plt.figure(figsize=(8, 8))
@@ -107,30 +106,46 @@ def run_cnn(split_directory, batchsize=256, epochs=50, color_mode='rgb'):
     plt.title('Training and Test Loss')
     plt.savefig('cnn_performance.png')
     plt.close()
-    
-    # Store curve details for ROC and PR Curve
+
+    # Reset Test Generator to align with dataset
+    test_data_gen.reset()
+
+    # Store data for results csv
+    labels = {1: "fishing", 0: "no_fishing"}
+    filenames=test_data_gen.filenames
     Y_confidence = model.predict_generator(test_data_gen).flatten()
     Y_pred = Y_confidence.round()
+    predictions = [labels[k] for k in Y_pred]
     Y_true = test_data_gen.classes
+    true_labels = [labels[k] for k in Y_true]
+    
+    # Create results csv
+    results=pd.DataFrame({"Filename":filenames, 
+                          "Confidence":Y_confidence, 
+                          "Predictions":predictions, 
+                          "True Label": true_labels})
+    results.to_csv("results.csv",index=False)
+    
+    # Store curve details for ROC and PR Curve
     fpr, tpr, thresholds = roc_curve(Y_true, Y_confidence)
     area = auc(fpr, tpr)
     precision, recall, thresholds = precision_recall_curve(Y_true, Y_confidence)
     
     # Plot ROC and PR Curve
     plt.figure(figsize=(8, 8))
-    plt.subplot(1, 2, 1)
     plt.plot([0, 1], [0, 1], 'k--')
     plt.plot(fpr, tpr, label=f'AUC = {area:.3f}')
     plt.xlabel('False positive rate')
     plt.ylabel('True positive rate')
     plt.title('ROC curve')
     plt.legend(loc='best')
+    plt.savefig('ROC.png')
+    plt.close()
 
-    plt.subplot(1, 2, 2)
     plt.plot(recall, precision, label=f'AUC = {area:.3f}')
     plt.xlabel('Recall')
     plt.ylabel('Precision')
     plt.title('Precision-Recall Curve')
     plt.legend(loc='best')
-    plt.savefig('ROC_PR.png')
+    plt.savefig('PR.png')
     plt.close()
